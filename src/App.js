4d8a4e673cee5a76d710b7bc89cfe98b3384e03a
John N. Norman <normanj777@gmail.com>
2022-07-17 14:22:46 -0400

    style with inline styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
 import React from "react";
 import mobileImage from "./images/image-product-mobile.jpg";
+import cartImage from "./images/icon-cart.svg";
+import "./app.css";
 
 export default function App() {
+  const fraunces = "'Fraunces', serif";
+  const montserrat = "'Montserrat', sans-serif";
   return (
-    <main className="app">
-      <div className="product-card">
-        <img src={mobileImage} alt="Chanel Perfume"></img>
-        <div className="content">
-          <p className="product-type">Perfume</p>
-          <h1 className="product-name">Gabrielle Essence Eau De Parfum</h1>
-          <p className="product-description">
+    <div
+      style={{
+        alignItems: "center",
+        backgroundColor: "#F2EAE2",
+        display: "flex",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <main
+        style={{
+          backgroundColor: "white",
+          borderRadius: 10,
+          overflow: "hidden",
+          width: 343,
+        }}
+      >
+        <img src={mobileImage} alt="a bottle of Chanel perfume" />
+        <div style={{ padding: 24 }}>
+          <p
+            style={{
+              color: "#6C7289",
+              fontFamily: montserrat,
+              fontWeight: "400",
+              fontSize: 12,
+              letterSpacing: 5,
+              lineHeight: "15px",
+              textTransform: "uppercase",
+            }}
+          >
+            Perfume
+          </p>
+          <h1
+            style={{
+              color: "#1C232B",
+              fontFamily: fraunces,
+              fontSize: 32,
+              fontWeight: "900",
+              lineHeight: "32px",
+              marginTop: 13,
+            }}
+          >
+            Gabrielle Essence Eau De Parfum
+          </h1>
+          <p
+            style={{
+              color: "#6C7289",
+              fontFamily: montserrat,
+              fontSize: 14,
+              fontWeight: "400",
+              lineHeight: "23px",
+            }}
+          >
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
-          <p className="price">$149.99</p>
-          <p className="original-price">$169.99</p>
-          <button className="add-button">Add to Cart</button>
+          <div style={{ display: "flex", marginTop: 23, marginBottom: 12 }}>
+            <p
+              style={{
+                color: "#3D8168",
+                fontFamily: fraunces,
+                fontSize: 32,
+                fontWeight: "900",
+                lineHeight: "32px",
+              }}
+            >
+              $149.99
+            </p>
+            <p
+              style={{
+                color: "#6C7289",
+                fontFamily: montserrat,
+                fontSize: 13,
+                fontWeight: "400",
+                marginLeft: 24,
+                textDecoration: "line-through",
+              }}
+            >
+              $169.99
+            </p>
+          </div>
+          <button
+            style={{
+              alignItems: "center",
+              backgroundColor: "#3D8168",
+              border: "none",
+              borderRadius: 8,
+              display: "flex",
+              color: "white",
+              fontFamily: montserrat,
+              height: 48,
+              justifyContent: "center",
+              outline: "none",
+              width: "100%",
+            }}
+          >
+            <img src={cartImage} alt="" style={{ marginRight: 11.61 }}></img>
+            <p>Add to Cart</p>
+          </button>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
