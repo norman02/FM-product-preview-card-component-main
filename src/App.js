87c5d6c64a7cd4abc9bd4945847a4fd2c3f5dc27
John N. Norman <normanj777@gmail.com>
2022-09-27 12:23:51 -0400

    add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import cartImage from "./images/icon-cart.svg";
 
 export default function App() {
   return (
-    <div>
-      <img src={mobileImage} alt="Chanel perfume" />
-      <p>Perfume</p>
-      <h1>Gabrielle Essence Eau De Parfum</h1>
-      <p>
-        A floral, solar and voluptuous interpretation composed by Olivier Polge,
-        Perfumer-Creator for the House of CHANEL.
-      </p>
-      <p>$149.99</p>
-      <p>$169.99</p>
-      <button>
-        <img src={cartImage} alt="" /> Add to Cart
-      </button>
-    </div>
+    <main>
+      <div className="card m-auto">
+        <img className="card-img-top" src={mobileImage} alt="Chanel perfume" />
+        <div className="card-body p-4">
+          <p className="text-uppercase">Perfume</p>
+          <h1 className="fs-1 fw-bold">Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <p className="price text-success fs-1 fw-bold">$149.99</p>
+          <p>$169.99</p>
+
+          <div className="d-grid">
+            <button className="btn btn-success fw-bold" type="button">
+              <img src={cartImage} alt="" /> Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
